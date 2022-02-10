1805ba903e962ee4d4fa9dc3fd4bd163902b841e
Anton Kechashin <atk@sitecore.net>
2022-02-10 17:03:26 +0200

    #516589 | Add missing Headless container component - background image support

--- samples/nextjs/src/components/Container.tsx
+++ samples/nextjs/src/components/Container.tsx
@@ -3,6 +3,7 @@ import {
   RenderingVariantParameters,
   ComponentRendering,
   Placeholder,
+  useSitecoreContext,
 } from '@sitecore-jss/sitecore-jss-nextjs';
 
 interface Fields {
@@ -17,13 +18,19 @@ interface ComponentProps {
 const Container = (props: ComponentProps): JSX.Element => {
   var styles = `${props.params.GridParameters} ${props.params.Styles?.replace(/\|/g, ' ')}`;
   var phKey = `container-${props.params.DynamicPlaceholderId}`;
+  var backgroundStyle;
+  var backgroundImage = props.params.BackgroundImage as string;
+  if (backgroundImage) {
+    var prefix = `${useSitecoreContext().sitecoreContext.pageState !== 'normal' ? '/sitecore/shell' : ''}/-/media/`
+    backgroundStyle = { backgroundImage: `url('${prefix}${backgroundImage?.match(/\{([^)]+)\}/)?.pop()?.replace(/\-/g,'')}')` };
+  }
   if (props.rendering.placeholders && props.rendering.placeholders["container-{*}"]) {
     props.rendering.placeholders[phKey] = props.rendering.placeholders["container-{*}"];
     delete props.rendering.placeholders["container-{*}"];
   }
   return (
     <div className={`component container ${styles}`}>
-      <div className="component-content">
+      <div className="component-content" style={backgroundStyle}>
         <div className="row">
           <Placeholder name={phKey} rendering={props.rendering} />
         </div>
